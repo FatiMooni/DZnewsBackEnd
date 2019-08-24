d1aafd98558cba4b2fea01bf70e5591b01157b0b
FatiMooni <ff_abdiche@esi.dz>
2019-08-24 20:20:29 +0200

    changes on post

--- app/controllers/article.controller.js
+++ app/controllers/article.controller.js
@@ -11,3 +11,35 @@ exports.findAll = (req, res) => {
         });
     });
 };
+
+// Create and Save a new article
+exports.create = (req, res) => {
+    // Validate request
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "article content can not be empty"
+        });
+    }
+
+    // Create a Note
+    const article = new article({
+        title: req.body.title || "Untitled Article", 
+        id : req.body.id,
+        theme : req.body.theme,
+        resume : req.body.resume,
+        author : req.body.author,
+        date : req.body.date,
+        uri : req.body.uri,
+        content: req.body.content
+    });
+
+    // Save Note in the database
+    article.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the article."
+        });
+    });
+};
