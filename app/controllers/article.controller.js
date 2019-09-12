7c0b8fa75f4e9d22dffe2361dc69b73b223b740d
FatiMooni <ff_abdiche@esi.dz>
2019-09-12 17:55:27 +0200

    deleteArticle1.5

--- app/controllers/article.controller.js
+++ app/controllers/article.controller.js
@@ -65,7 +65,7 @@ exports.findOne = (req, res) => {
 
 exports.delete = (req, res) => {
     //User.   req.params.id
-    Article.findOneAndRemove({ articleID : req.params.article_id})
+    Article.findOneAndRemove({ articleID : req.params.article_id , userID : req.params.id})
     .then(article => {
         if(!article) {
             return res.status(404).send({
